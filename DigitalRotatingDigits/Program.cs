db5664508085fefabf7bc4315fd63737d2c95d5a
DerGuteste <damian.gbs@grabrovec.de>
2023-06-01 16:12:37 +0200

    correction & more artistic freedom
    freedom due to string instead of short

--- DigitalRotatingDigits/Program.cs
+++ DigitalRotatingDigits/Program.cs
@@ -5,92 +5,96 @@
         /// <summary>
         /// 1: | 2: _ 3: ‾
         /// </summary>
-        static short[][,] digits = new short[][,]
+        static string[][] digits = new string[][]
         {
-            new short[4, 3] //0
+            new string[4] //0
             {
-                {0, 2, 0},
-                {1, 0, 1},
-                {1, 0, 1},
-                {0, 3, 0}
+                " _ ",
+                "| |",
+                "| |",
+                " ¯ "
             },
-            new short[4, 3] //1
+            new string[4] //1
             {
-                {0, 0, 0},
-                {0, 0, 1},
-                {0, 0, 1},
-                {0, 0, 0}
+                "   ",
+                "|  ",
+                "|  ",
+                "   "
             },
-            new short[4, 3] //2
+            new string[4] //2
             {
-                {0, 2, 0},
-                {0, 2, 1},
-                {1, 0, 0},
-                {0, 3, 0}
+                " _ ",
+                " _|",
+                "|  ",
+                " ¯ "
             },
-            new short[4, 3] //3
+            new string[4] //3
             {
-                {0, 2, 0},
-                {0, 2, 1},
-                {0, 0, 1},
-                {0, 3, 0}
+                " _ ",
+                " _|",
+                "  |",
+                " ¯ "
             },
-            new short[4, 3] //4
+            new string[4] //4
             {
-                {0, 0, 0},
-                {1, 2, 1},
-                {0, 0, 1},
-                {0, 0, 0}
+                "   ",
+                "|_|",
+                "  |",
+                "   "
             },
-            new short[4, 3] //5
+            new string[4] //5
             {
-                {0, 2, 0},
-                {1, 2, 0},
-                {0, 0, 1},
-                {0, 3, 0}
+                " _ ",
+                "|_ ",
+                "  |",
+                " ¯ "
             },
-            new short[4, 3] //6
+           new string[4] //6
             {
-                {0, 2, 0},
-                {1, 2, 0},
-                {1, 0, 1},
-                {0, 3, 0}
+                " _ ",
+                "|_ ",
+                "| |",
+                " ¯ "
             },
-            new short[4, 3] //7
+            new string[4] //7
             {
-                {0, 2, 0},
-                {0, 0, 1},
-                {0, 0, 1},
-                {0, 0, 0}
+                " _ ",
+                "  |",
+                "  |",
+                "   "
             },
-            new short[4, 3] //8
+            new string[4] //8
             {
-                {0, 2, 0},
-                {1, 2, 1},
-                {1, 0, 1},
-                {0, 3, 0}
+                " _ ",
+                "|_|",
+                "| |",
+                " ¯ "
             },
-            new short[4, 3] //9
+            new string[4] //9
             {
-                {0, 2, 0},
-                {1, 2, 1},
-                {0, 0, 1},
-                {0, 0, 0}
+                " _ ",
+                "|_|",
+                "  |",
+                "   "
             },
-            new short[,] // stick man
+            new string[7] //2
             {
-                {0, 0, 2, 0, 0 },
-                {0, 1, 0, 1, 0 },
-                {0, 0, 3, 0, 1 },
-                {3, 3, 1, 3, 0 },
-                {0, 0, 1, 0, 0 },
-                {0, 1, 0, 1, 0 },
-                {0, 1, 0, 1, 0 },
-            }
+                "  _  ",
+                " | | ",
+                "  ¯ |",
+                "¯¯|¯ ",
+                "  |  ",
+                " | | ",
+                " | | "
+            },
         };
+
 
         static void Main(string[] args)
         {
+            int height = 4;
+            int width = 16;
+
             #region get input
 
             string input;
@@ -129,9 +133,13 @@
 
             #endregion
 
+            Console.Clear();
+
             // no need to allocate every itteration
             ConsoleKeyInfo keyinfo;
             int index;
+            int stX = Console.CursorLeft;
+            int stY = Console.CursorTop;
 
             while (true)
             {
@@ -144,8 +152,6 @@
 
                 #endregion
 
-                Console.CursorLeft = 0;
-
                 #region rotate
 
                 keyinfo = Console.ReadKey();
@@ -155,6 +161,15 @@
                     {
                         index = input[i] - '0';
                         digits[index] = dRotX(digits[index]);
+                    }
+
+                    if(stY == height)
+                    {
+                        stY = 0;
+                    }
+                    else
+                    {
+                        stY = height;
                     }
                 }
                 else if (keyinfo.Key == ConsoleKey.Y)
@@ -166,61 +181,99 @@
                     }
 
                     input = sRotY(input);
+
+                    if(stX == width)
+                    {
+                        stX = 0;
+                    }
+                    else
+                    {
+                        stX = width;
+                    }
                 }
 
                 #endregion
 
-                Console.CursorLeft = 0;
+
+                Console.Clear();
+                Console.CursorLeft = stX;
+                Console.CursorTop = stY;
             }
 
             #region stick man
 
-            //while (true)
-            //{
-            //    printDigit(digits[10]);
+            height = 7;
+            width = 5;
 
-            //    Console.CursorLeft = 0;
+            while (true)
+            {
+                printDigit(digits[10]);
 
-                  #region rotate
+                #region rotate
 
-            //    keyinfo = Console.ReadKey();
+                keyinfo = Console.ReadKey();
 
-            //    if (keyinfo.Key == ConsoleKey.X)
-            //    {
-            //        digits[10] = dRotX(digits[10]);
-            //    }
-            //    else if (keyinfo.Key == ConsoleKey.Y)
-            //    {
-            //        digits[10] = dRotY(digits[10]);
+                if (keyinfo.Key == ConsoleKey.X)
+                {
+                    digits[10] = dRotX(digits[10]);
 
-            //    }
+                    if (stY == height)
+                    {
+                        stY = 0;
+                    }
+                    else
+                    {
+                        stY = height;
+                    }
+                }
+                else if (keyinfo.Key == ConsoleKey.Y)
+                {
+                    digits[10] = dRotY(digits[10]);
 
-                  #endregion
+                    if (stX == width)
+                    {
+                        stX = 0;
+                    }
+                    else
+                    {
+                        stX = width;
+                    }
+                }
 
-            //    Console.CursorLeft = 0;
-            //}
+                #endregion
+
+                Console.Clear();
+                Console.CursorLeft = stX;
+                Console.CursorTop = stY;
+            }
 
             #endregion
 
         }
 
         /// <summary>
-        /// rotate the digit on the Y-Axis
+        /// mirrors a string
         /// </summary>
-        /// <param name="digit"></param>
+        /// <param name="str"></param>
         /// <returns></returns>
-        static short[,] dRotY(short[,] digit)
+        static string[] dRotX(string[] digit)
         {
-            int x = digit.GetLength(0);
-            int y = digit.GetLength(1);
+            string[] output = new string[digit.Length];
 
-            short[,] output = new short[x, y];
-
-            for (int i = 0; i < x; i++)
+            for(int i = 0; i < digit.Length; i++)
             {
-                for (int j = 0; j < y; j ++)
+                output[i] = digit[digit.Length - i - 1];
+
+                for(int j = 0; j < output[i].Length; j++)
                 {
-                    output[i, j] = digit[i, y - j - 1];
+                    if (output[i][j] == '_')
+                    {
+                        output[i] = output[i].Remove(j, 1).Insert(j, "¯");
+                    }
+                    else if (output[i][j] == '¯')
+                    {
+                        output[i] = output[i].Remove(j, 1).Insert(j, "_");
+                    }
                 }
             }
 
@@ -228,31 +281,17 @@
         }
 
         /// <summary>
-        /// rotate the digit on the X-Axis
+        /// mirrors a string
         /// </summary>
-        /// <param name="digit"></param>
+        /// <param name="str"></param>
         /// <returns></returns>
-        static short[,] dRotX(short[,] digit)
+        static string[] dRotY(string[] digit)
         {
-            int x = digit.GetLength(0);
-            int y = digit.GetLength(1);
+            string[] output = new string[digit.Length];
 
-            short[,] output = new short[x, y];
-
-            for (int i = 0; i < x; i++)
+            for(int i = 0; i < digit.Length; i++)
             {
-                for (int j = 0; j < y; j++)
-                {
-                    output[i, j] = digit[x - i - 1, j];
-                    if (output[i, j] == 2)
-                    {
-                        output[i, j] = 3;
-                    }
-                    else if(output[i, j] == 3)
-                    {
-                        output[i, j] = 2;
-                    }
-                }
+                output[i] = sRotY(digit[i]);
             }
 
             return output;
@@ -279,37 +318,20 @@
         /// print a digit in "digital clock style"
         /// </summary>
         /// <param name="digit"></param>
-        static void printDigit(short[,] digit)
+        static void printDigit(string[] digit)
         {
             int posX = Console.CursorLeft;
             int posY = Console.CursorTop;
 
-            int x = digit.GetLength(0);
-            int y = digit.GetLength(1);
+            int x = digit.Length;
+            int y = digit[0].Length;
 
             for(int i = 0; i < x; i++)
             {
                 Console.CursorTop = posY + i;
                 Console.CursorLeft = posX;
-
-                for (int j = 0; j < y; j++)
-                {
-                    switch (digit[i, j])
-                    {
-                        case 0:
-                            Console.Write(' ');
-                            break;
-                        case 1:
-                            Console.Write('|');
-                            break;
-                        case 2:
-                            Console.Write('_');
-                            break;
-                        case 3:
-                            Console.Write('\u0305');
-                            break;
-                    }
-                }
+                
+                Console.Write(digit[i]);
             }
 
             Console.CursorTop = posY;
